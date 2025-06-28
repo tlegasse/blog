eda70321370fef85a9f50cae915ba89ac5fb5225
tlegasse <tlegasse@users.noreply.github.com>
2025-06-28 23:00:36 +0000

    deploy: afa1c7bdb33cdbf2bf720288b0552e54864b3ffa

---
+++ projects/sin-cosin/anim2.js
@@ -0,0 +1,45 @@
+(() => {
+    /** @type {HTMLCanvasElement} */
+    const canvas = document.getElementById('c2');
+    const canvasDims = canvas.getBoundingClientRect()
+    canvas.width = canvasDims.width
+    canvas.height = canvasDims.height
+
+    /** @type {CanvasRenderingContext2D} */
+    const ctx = canvas.getContext('2d')
+
+    const timeCoefficient = 600
+
+    ctx.fillStyle = "#ffffff"
+    ctx.fillRect(0, 0, canvasDims.width, canvasDims.height)
+
+    const getFrame = (timestamp) => {
+        ctx.fillStyle = "#ffffff29"
+        ctx.fillRect(0, 0, canvasDims.width, canvasDims.height)
+
+        const rad = 50
+
+        // Calculating the angular 
+        const particlePX = Math.cos(timestamp / timeCoefficient * 8) * (rad / 2)
+        const particlePY = Math.sin(timestamp / timeCoefficient * 8) * (rad / 2)
+
+        // calculating the x and y of the point based on radius
+        const pX = (Math.cos(timestamp / (timeCoefficient)) * rad) + canvasDims.width / 2
+        const pY = (Math.sin(timestamp / (timeCoefficient)) * rad) + canvasDims.height / 2
+
+        ctx.beginPath();
+        ctx.fillStyle = '#eeb549'
+        ctx.arc(pX + particlePX, pY + particlePY, 8, 0, 2 * Math.PI);
+        ctx.closePath()
+        ctx.fill()
+
+        ctx.beginPath();
+        ctx.fillStyle = '#00beff'
+        ctx.arc(pX, pY, 8, 0, 2 * Math.PI);
+        ctx.closePath()
+        ctx.fill()
+        requestAnimationFrame(getFrame)
+    }
+
+    requestAnimationFrame(getFrame)
+})()
